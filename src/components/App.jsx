204dd6ea93e30773d8481bbb19f61e573bdded00
KonstantinLypskyi <KonstantinLypskyi@gmail.com>
2018-07-25 23:57:17 +0300

    ''

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Header from './Header'
 import WeatherContainer from '../containers/WeatherContainer'
 import Footer from './Footer'
-// import TestContainer from '../containers/TestContainer'
 
 const App = () => {
     return (
--- src/containers/SearchContainer.jsx
+++ src/containers/SearchContainer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
 import { changeValue, searchFetchData, searchCity, choiseCity } from '../actions/search'
 import { weatherFetchData } from '../actions/weather'
 import Search from '../components/Search'
 
 class SearchContainer extends React.Component {
+  componentWillMount() {
+    document.addEventListener('click', this.unStrike, false)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.unStrike, false)
+  }
+
+  unStrike = event => {
+    ReactDOM.findDOMNode(this).contains(event.target) ? null : this.props.choiseCity()
+  }
 
   render() {
     return (
